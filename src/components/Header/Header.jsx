ef6847037979d4194580842b63c74a7a5766dcd8
Akash Khan <akashcc48@gmail.com>
2023-09-21 19:21:40 +0600

    header part added and router setuped

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/jobs">Jobs</NavLink>
+      </li>
+      <li>
+        <NavLink to="/applied">Applied Jobs</NavLink>
+      </li>
+      <li>
+        <NavLink to="/statics">Statics</NavLink>
+      </li>
+      <li>
+        <NavLink to="/applied">Blogs</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className=" backdrop-blur-sm bg-white/10">
+      <div className="navbar container mx-auto">
+        <div className="navbar-start ">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-xl dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {links}
+              <div className="navbar-end">
+                <a className="custom-btn">Button</a>
+              </div>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-3xl lg:text-5xl">
+            <span className="text-red-800">AS</span> Career
+          </a>
+        </div>
+        <div className="navbar-center  hidden lg:flex">
+          <ul className="menu menu-horizontal text-xl px-1">{links}</ul>
+        </div>
+        <div className="navbar-end hidden md:flex">
+          <a className="custom-btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
